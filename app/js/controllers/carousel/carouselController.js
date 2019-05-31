14e775d286bde95276c8841b1e3a8d3b71a2ab8c
edelmira.morataya <edelmorataya@gmail.com>
2019-05-31 15:45:26 -0600

    include carousel section

--- app/js/controllers/carousel/carouselController.js
+++ app/js/controllers/carousel/carouselController.js
@@ -2,11 +2,43 @@
 (() => {
     'use strict'
 
-    let homeModule = angular.module("mainApp");
+    let carouselModule = angular.module('mainApp');
 
     carouselModule.controller('carouselController', function(){
         let vm = this;
-        vm.test = 'carousel controller ';
+        vm.slide =[{
+            id: 0,
+            name: 'Imagen 1',
+            dato: 'Primer imagen',
+            image: 'img/1.jpg',
+        },
+        {
+            id: 1,
+            name: 'Imagen 2',
+            dato: 'segunda imagen',
+            image: 'img/2.jpg',
+        },
+        {
+            id: 2,
+            name: 'Imagen 3',
+            dato: 'tercera imagen',
+            image: 'img/3.jpg',
+        },
+        {
+            id: 3,
+            name: 'Imagen 4',
+            dato: 'Descripcion imagen',
+            image: 'img/4.jpg',
+        },
+        {
+            id: 4,
+            name: 'Imagen 5',
+            dato: 'Descripcion imagen',
+            image: 'img/5.jpg',
+        }
+
+    ];
+        
 
     });
 
