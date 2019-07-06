3d6ea013a062c8ba32418a142d260be63030ba3a
edelmorataya <edelmorataya@us-MacBook-Pro.local>
2019-07-05 22:48:14 -0600

    Custom Filter working properly

--- app/js/filters/agefilter.js
+++ app/js/filters/agefilter.js
@@ -5,13 +5,31 @@
     let module = angular.module("mainApp");
 
     module.filter('agefilter', function(){
-        return function (input){
-            if(input>=18){
-            return input
-            }
-        }
+        return function (items, edad){
+            
+            let filtered = [];
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                if(edad == "1"){
+                    if (item.age >= 18){
+                        filtered.push(item);
+                    }
+                }
+                if(edad == "2"){
+                    if (item.age <= 17){
+                        filtered.push(item);
+                    }
+                }
+                   console.log(filtered)
+             }
+        return filtered;
 
-    })
+     };
+
+    });
+
+
+
 
 })();
 
